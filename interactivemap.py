dc8a6129d1197fddc909f3a367e96eaaab2e0d7d
Kristin Aoki <you@example.com>
2019-03-13 13:58:17 -0400

    Change columns read for data to "Death Rates" from "Age-adjusted Death Rates"

--- interactivemap.py
+++ interactivemap.py
@@ -14,17 +14,17 @@ state_data = pd.read_csv(state_deaths)
 m = folium.Map(location=[48, -102], zoom_start=3)
 
 folium.Choropleth(geo_data=state_geo,name='choropleth',data=state_data,
-    columns=['State', 'Deaths', 'Cause Name', 'Age-adjusted Death Rate'],
+    columns=['State',  'Death Rate'],
     key_on='feature.id',
     fill_color='YlGn',
     fill_opacity=0.7,
     line_opacity=0.2,
-    legend_name='death Rate (%)'
+    legend_name='Death Rate (1999-2016)'
 ).add_to(m)
 
 folium.LayerControl().add_to(m)
 
 m.save('map.html')
 
-with open(NCHS_-_Leading_Causes_of_Death__United_States(1).csv) as csvf:
-    readCSV = csv.DictReader(csvf) #saving csv data to a dictionary
+# with open(NCHS_-_Leading_Causes_of_Death__United_States(1).csv) as csvf:
+#     readCSV = csv.DictReader(csvf) #saving csv data to a dictionary
