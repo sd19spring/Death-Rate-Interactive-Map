dd03b6e844185067ef8938eff8c28604f74eb65f
Kristin Aoki <you@example.com>
2019-03-15 17:39:40 -0400

    Change the map colors and increased line thickness

--- interactivemap.py
+++ interactivemap.py
@@ -1,24 +1,28 @@
 "Interactive Map of Deaths in United States (1999-2016)"
 "@Autors: Kristin Aoki and Sina "
 
+
 #--------------------------------------------------------------------------------------
 import csv
 import pandas as pd
 import os
 import folium
 
+
 state_geo = os.path.join('data', 'us_states.json')
 state_deaths = os.path.join('data','death.csv')
 state_data = pd.read_csv(state_deaths)
 
+#establishes the center of map based on longitude and latitude
 m = folium.Map(location=[50.246366, -110], zoom_start=4)
 
+#sets the color scheme, data used for legend, and legend labels
 folium.Choropleth(geo_data=state_geo,name='choropleth',data=state_data,
     columns=['State',  'Death Rate'],
     key_on='feature.id',
-    fill_color='YlGn',
+    fill_color='BuPu',
     fill_opacity=0.7,
-    line_opacity=0.2,
+    line_opacity=0.5,
     legend_name='Average Death Rate (1999-2016)'
 ).add_to(m)
 
