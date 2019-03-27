bc620242a43fc70efbc653d27c59e4e9b1635ff5
Kristin Aoki <you@example.com>
2019-03-27 15:54:56 -0400

    Add marker data and updated files that contain popup data

--- interactivemap.py
+++ interactivemap.py
@@ -9,13 +9,14 @@ import pandas as pd
 import os
 import folium
 from folium import Choropleth, Map, Marker, LayerControl
+from folium import IFrame
 
 
 
 state_geo = os.path.join('data', 'us_states.json')
 state_deaths = os.path.join('data','death.csv')
 state_data = pd.read_csv(state_deaths)
-marker_data = os.path.join('data', 'State Long and Lat.csv')
+marker_data = os.path.join('data', 'StateLonandLat1999-2016.csv')#need to make this a more universal statement because each each has own file
 marker_coord = pd.read_csv(marker_data)
 #establishes the center of map based on longitude and latitude
 m = Map(location=[50.246366, -110], zoom_start=4)
@@ -31,29 +32,19 @@ class AverageMap:
         legend_name='Average Death Rate (1999-2016)'
         ).add_to(m)
     for i in range(0,len(marker_coord)):
-        Marker([marker_coord.iloc[i]['lon'],marker_coord.iloc[i]['lat']], popup=marker_coord.iloc[i]['state']).add_to(m)
+        popup = folium.Popup(IFrame(marker_coord.iloc[i]['state'],width=350,height=250))
+        popup2 = folium.Popup(marker_coord.iloc[i]['state'],max_width=350)
+        Marker([marker_coord.iloc[i]['lon'],marker_coord.iloc[i]['lat']], popup=popup2).add_to(m)
     LayerControl().add_to(m)
 
-class Popup():
-    'attributions:states, average number of deaths, location'
-
-# class Popup:
-#     with open(filename) as csvf:
-#         readCSV = csv.DictReader(csvf) #saving csv data to a dictionary
-def sort_state_data(filename):
-    with open(filename, 'r')as f:
-        csv1 = csv.reader(f, delimiter=',')
-        sort = sorted(csv1,key=operator.itemgetter(5))
-        #sort = sorted(csv1,key=lambda average: average[5])
-        return sort
-
-def popup_content(file1,file2):
-    with open(file1,'r') as f1:
-        df = pandas.read_csv(f1, index_col = 0)
-        df.ix[2:3,]
-
-
+# class Popup():
+#     'attributions:states, average number of deaths, location'
+#     def popup_size():
+#         popup = folium.Popup(marker_coord.iloc[i]['state'],max_width=100)
+#
+#     def marker():
+#         for i in range(0,len(marker_coord)):
+#             Marker([marker_coord.iloc[i]['lon'],marker_coord.iloc[i]['lat']], popup=popup).add_to(m)
 
 if __name__ == "__main__":
-    m.save('AverageMapmap.html')
-    print(sort_state_data('Alabama.csv'))
+    m.save('AverageMap.html')
